9c309c415a7c977523cc46426e518c2a3399d2f1
tbiuk <toni.biuk@gmail.com>
2018-04-05 12:16:14 +0200

    Pthread trial

---
+++ Seminar_1/code/1_pthread_app/inc/Random.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include <random>
+
+#include <algorithm>
+
+class Random
+{
+public:
+	Random();
+	explicit Random(uint32_t seed);
+
+	int32_t Next() const;
+	int32_t Next(int32_t ceiling) const;
+	int32_t Next(int32_t floor, int32_t ceiling) const;
+
+	double NextDouble() const;
+	float NextFloat() const;
+
+	template <typename T, size_t Size>
+	void ShuffleArray(T(&array)[Size]);
+
+private:
+	std::default_random_engine& globalURNG() const;
+	void Randomize() const;
+	void Randomize(uint32_t seed) const;
+};
+
+template <typename T, size_t Size>
+void Random::ShuffleArray(T(&array)[Size])
+{
+	for (auto& element : array)
+	{
+		auto r = Next(0, Size - 1);
+		std::swap(element, array[r]);
+	}
+}
