e5d7f69452efa8a24b9e0a5227523ab609304670
sugario <tmelva00@fesb.hr>
2018-05-20 19:02:10 +0200

    Changing the way array size is calculated in example 3.

--- Seminar_2/Example_3/src/main.cpp
+++ Seminar_2/Example_3/src/main.cpp
@@ -9,23 +9,23 @@
 #define KILOBYTE        (1024U * BYTE)
 #define MEGABYTE        (1024U * KILOBYTE)
 
-#define L3_CACHE_SIZE   (9U * MEGABYTE)
-#define ARR_LENGTH      (32U * KILOBYTE)
+#define ARR_MEMORY_SIZE (10U * MEGABYTE)
+#define ARR_LENGTH      (ARR_MEMORY_SIZE / (sizeof(int32_t) * BYTE))
 
 int32_t main() {
     Clock clock;
 
-    const auto steps = L3_CACHE_SIZE;
+    const auto steps = 64U * 1024U * 1024U;
     auto *arr = new int32_t[ARR_LENGTH]();
 
     INFO("Array size: %u\n", ARR_LENGTH);
-    clock.Start();
     for (auto i = 0U; i < steps; i++) {
+        clock.Start();
         arr[(i * 16) % ARR_LENGTH]++;
+        const auto elapsedNS = clock.ElapsedNanoSeconds();
+        INFO("Time per element: %lld ns\n", elapsedNS);
     }
 
-    const auto elapsedNS = clock.ElapsedNanoSeconds();
-    INFO("Time per element: %lld ns\n", elapsedNS / ARR_LENGTH);
 
     delete[] arr;
 
