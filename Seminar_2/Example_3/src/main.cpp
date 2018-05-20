ad1c2452b03179033a42e0d448b8f6090dd49874
sugario <tmelva00@fesb.hr>
2018-05-20 23:27:57 +0200

    Adding optimization to example 3.

--- Seminar_2/Example_3/src/main.cpp
+++ Seminar_2/Example_3/src/main.cpp
@@ -9,7 +9,7 @@
 #define KILOBYTE        (1024U * BYTE)
 #define MEGABYTE        (1024U * KILOBYTE)
 
-#define ARR_MEMORY_SIZE (10U * MEGABYTE)
+#define ARR_MEMORY_SIZE (64U * KILOBYTE)
 #define ARR_LENGTH      (ARR_MEMORY_SIZE / (sizeof(int32_t) * BYTE))
 
 int32_t main() {
@@ -18,14 +18,14 @@ int32_t main() {
     const auto steps = 64U * 1024U * 1024U;
     auto *arr = new int32_t[ARR_LENGTH]();
 
-    INFO("Array size: %u\n", ARR_LENGTH);
+    INFO("Array size: %llu\n", ARR_LENGTH);
+    clock.Start();
     for (auto i = 0U; i < steps; i++) {
-        clock.Start();
-        arr[(i * 16) % ARR_LENGTH]++;
-        const auto elapsedNS = clock.ElapsedNanoSeconds();
-        INFO("Time per element: %lld ns\n", elapsedNS);
+        arr[(i * 16U) % ARR_LENGTH]++;
     }
 
+    const auto elapsedNS = clock.ElapsedNanoSeconds();
+    INFO("Time per element: %lld ns\n", elapsedNS / steps);
 
     delete[] arr;
 
