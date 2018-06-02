78301aa9e29bf7b93558834f8cac43adc9f62763
sugario <tmelva00@fesb.hr>
2018-06-02 15:49:03 +0200

    Removing CUDA placeholder.

--- Seminar_3/GaborFilter/GaborFilter/inc/Image/Convolution.hpp
+++ Seminar_3/GaborFilter/GaborFilter/inc/Image/Convolution.hpp
@@ -6,8 +6,6 @@
 namespace Convolution {
     cv::Mat Sequential(const cv::Mat &image, const cv::Mat &kernel);
     cv::Mat Parallel(const cv::Mat &image, const cv::Mat &kernel);
-    // cv::Mat CUDA(const cv::Mat &image, const cv::Mat &kernel);
-    
 }
 
 #endif  // !IMAGE_CONVOLUTION_HPP
