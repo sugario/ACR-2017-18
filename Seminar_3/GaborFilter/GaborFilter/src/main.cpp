5c770d5052182392ff48cbbd7df261e6953b905a
sugario <tmelva00@fesb.hr>
2018-06-05 19:03:24 +0200

    Minor changes to main. Adding time mesuring.

--- Seminar_3/GaborFilter/GaborFilter/src/main.cpp
+++ Seminar_3/GaborFilter/GaborFilter/src/main.cpp
@@ -2,6 +2,7 @@
 #include <Image/Convolution_CUDA.cuh>
 #include <Image/Image.hpp>
 #include <Filter/GaborFilter.hpp>
+#include <Utility/Stopwatch.hpp>
 
 #include <cstdint>
 #include <cstdlib>
@@ -11,11 +12,10 @@
 #define SYNTHETIC           "assets/synthetic1.png"
 #define BIG_PICTURE         "assets/big_picture.png"
 
-#define RESULT_FILE_NAME    "result.png"
-
 #define PATH_TO_IMAGE       (RAINBOW)
 
 int32_t main() {
+    Stopwatch stopwatch;
     Image image(PATH_TO_IMAGE, cv::IMREAD_GRAYSCALE);
     image.FormatItself(CV_32F);
 
@@ -23,27 +23,42 @@ int32_t main() {
     filter.RefreshKernel();
 
     // Sequential
+    stopwatch.Start();
+
     const auto convResultSequential = Convolution::Sequential(image.GetData(),
                                                               filter.kernel);
     Image resultSequential(convResultSequential);
     resultSequential.FormatItself(CV_8U, 1.0 / 255.0);
     resultSequential.WriteToFile("resultSequential.png");
+
+    stopwatch.Pause();
+    printf("Sequential:\t%lld ms\n", stopwatch.ElapsedMiliseconds());
     // !Sequential
 
     // Parallel
+    stopwatch.Restart();
+
     const auto convResultParallel = Convolution::Parallel(image.GetData(),
                                                           filter.kernel);
     Image resultParallel(convResultParallel);
     resultParallel.FormatItself(CV_8U, 1.0 / 255.0);
     resultParallel.WriteToFile("resultParallel.png");
+
+    stopwatch.Pause();
+    printf("Parallel:\t%lld ms\n", stopwatch.ElapsedMiliseconds());
     // !Parallel
 
     // CUDA
+    stopwatch.Restart();
+
     const auto convResultCUDA = Convolution::CUDA(image.GetData(),
                                                   filter.kernel);
     Image resultCUDA(convResultCUDA);
     resultCUDA.FormatItself(CV_8U, 1.0 / 255.0);
     resultCUDA.WriteToFile("resultCUDA.png");
+
+    stopwatch.Pause();
+    printf("CUDA:\t\t%lld ms\n", stopwatch.ElapsedMiliseconds());
     // !CUDA
 
     return EXIT_SUCCESS;
